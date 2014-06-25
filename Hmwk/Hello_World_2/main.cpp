662762a16379f4bb314379885458104c269965cd
Idalia Ramirez <iramirez32@student.rcc.edu>
2014-06-25 09:47:00 -0700

    Signed-off-by: Idalia Ramirez <iramirez32@student.rcc.edu>

---
+++ Hmwk/Hello_World_2/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Idalia R
+ *Created on June 23, 2014, 11:31 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout<<"Hello World!"<<endl;
+    return 0;
+}
+
