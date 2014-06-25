e01e7ffd7986524ea80c2d66f923345088927ea3
Idalia Ramirez <iramirez32@student.rcc.edu>
2014-06-25 14:05:55 -0700

    Signed-off-by: Idalia Ramirez <iramirez32@student.rcc.edu>

---
+++ Hmwk/Assignment 1/Savitch_8thEd_Ch1_Prob1_Pg35/main.cpp
@@ -0,0 +1,32 @@
+/* 
+ * File:   main.cpp
+ * Author: Idalia Ramirez
+ * Created on June 25, 2014, 12:02 PM
+ */
+#include <iostream>
+using namespace std;
+
+int main( ) {
+    int Nmofpds, psprpd, totps;
+    
+    cout <<"Press return after entering a number.\n";
+    cout <<"Enter the number of pods:\n";
+    
+    cin >> Nmofpds;
+    
+    cout << "Enter the number of peas in a pod;\n";
+    cin >> psprpd;
+    totps = Nmofpds * psprpd;
+    cout << "If you have ";
+    cout << Nmofpds;
+    cout << " pea pods\n";
+    cout << "and ";
+    cout << psprpd;
+    cout << " peas in each pod, then.\n";
+    cout << "you have ";
+    cout << totps;
+    cout << " peas in all the pods.\n";
+
+    return 0;
+}
+
--- Hmwk/Assignment 1/Savitch_8thEd_Ch1_Prob2_Pg35/main.cpp
+++ Hmwk/Assignment 1/Savitch_8thEd_Ch1_Prob2_Pg35/main.cpp
@@ -1,31 +1,35 @@
 /* 
  * File:   main.cpp
  * Author: Idalia Ramirez
- * Created on June 25, 2014, 12:02 PM
+ * Created on June 25, 2014, 1:27 PM
  */
 #include <iostream>
 using namespace std;
 
 int main( ) {
-    int number_of_pods, peas_per_pod, total_peas;
     
+    int Nmofpds, psprpd, totps;
+    
+    cout <<"Hello!\n";
     cout <<"Press return after entering a number.\n";
     cout <<"Enter the number of pods:\n";
     
-    cin >> number_of_pods;
+    cin >> Nmofpds;
     
-    cout << "Enter the number of peas in a pod;\n";
-    cin >> peas_per_pod;
-    total_peas = number_of_pods * peas_per_pod;
+    cout << "Enter the number of peas in a pod:\n";
+    cin >> psprpd;
+    totps = Nmofpds * psprpd;
     cout << "If you have ";
-    cout << number_of_pods;
+    cout << Nmofpds;
     cout << " pea pods\n";
     cout << "and ";
-    cout << peas_per_pod;
-    cout << " peas in each pod, then.\n";
+    cout << psprpd;
+    cout << " peas in each pod, then\n";
     cout << "you have ";
-    cout << total_peas;
+    cout << totps;
     cout << " peas in all the pods.\n";
+    cout << "Good-bye!\n";
+
 
     return 0;
 }
