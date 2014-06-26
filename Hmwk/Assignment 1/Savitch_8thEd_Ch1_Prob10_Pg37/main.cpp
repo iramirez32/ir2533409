189406a2810a17f1f3515724aae022a37a1f4abe
Idalia Ramirez <iramirez32@student.rcc.edu>
2014-06-26 12:55:38 -0700

    Signed-off-by: Idalia Ramirez <iramirez32@student.rcc.edu>

---
+++ Hmwk/Assignment 1/Savitch_8thEd_Ch1_Prob10_Pg37/main.cpp
@@ -0,0 +1,39 @@
+/* 
+ * File:   main.cpp
+ * Author: Idalia Ramirez
+ * Created on June 26, 2014, 12:30 PM
+ */
+#include <iostream>
+using namespace std;
+//System Libraries
+
+//User Defined Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execute Here!
+int main(int argc, char** argv) {
+    //Define variables
+    char ltr; //Short for letter
+    
+    cout<<"Enter your favorite letter.\n";
+    cin>>ltr;
+    
+    cout<<"    "<<ltr<<" "<<ltr<<" "<<ltr<<endl;
+    cout<<"   "<<ltr<<"     "<<ltr<<endl;
+    cout<<"  "<<ltr<<endl;
+    cout<<"  "<<ltr<<endl;
+    cout<<"  "<<ltr<<endl;
+    cout<<"  "<<ltr<<endl;
+    cout<<"  "<<ltr<<endl;
+    cout<<"   "<<ltr<<"     "<<ltr<<endl;
+    cout<<"    "<<ltr<<" "<<ltr<<" "<<ltr<<"\n"<<endl;
+    
+    cout<<"Surprise. Carry on now.";
+
+    //End Here.
+    return 0;
+}
+
