6e8773a4bf94a655b32c6bf212185cc2a08027e1
Idalia Ramirez <iramirez32@student.rcc.edu>
2014-06-25 19:31:35 -0700

    Signed-off-by: Idalia Ramirez <iramirez32@student.rcc.edu>

---
+++ Hmwk/Assignment 1/Savitch_8thEd_Ch1_Prob7_Pg36/main.cpp
@@ -0,0 +1,28 @@
+/* 
+ * File:   main.cpp
+ * Author: Idalia Ramirez
+ * Created on June 25, 2014, 7:14 PM
+ */
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+    
+    cout <<"********************************************\n";
+    
+    cout <<"     C C C          S S S S          !!\n";
+    cout <<"    C     C        S        S        !!\n";
+    cout <<"   C              S                  !!\n";
+    cout <<"  C                S                 !!\n";
+    cout <<"  C                 S S S S          !!\n";
+    cout <<"  C                         S        !!\n";
+    cout <<"   C                         S       !!\n";
+    cout <<"    C      C       S         S   \n";
+    cout <<"      C C C          S S S S         00\n";;
+    
+    cout <<"*******************************************\n";
+    
+    cout <<" Computer Science is Cool Stuff!!!\n";
+    return 0;
+}
+
