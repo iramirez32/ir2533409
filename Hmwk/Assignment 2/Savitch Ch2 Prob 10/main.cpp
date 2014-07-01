e1622d42a5a856fa961a872d573b3e5f9f16e314
Idalia Ramirez <iramirez32@student.rcc.edu>
2014-06-30 22:15:45 -0700

    Signed-off-by: Idalia Ramirez <iramirez32@student.rcc.edu>

--- Hmwk/Assignment 2/Savitch Ch2 Prob 10/main.cpp
+++ Hmwk/Assignment 2/Savitch Ch2 Prob 10/main.cpp
@@ -3,27 +3,34 @@
  * Author: Idalia Ramirez
  * Created on June 30, 2014, 9:02 PM
  */
+//System Libraries
 #include <iostream>
 using namespace std;
+//User Defined Libraries
 
+//Global Constants
 
+//Function Prototypes
+
+//Execute Here!
 int main(int argc, char** argv) {
     //Define variables
-    int num,
-            sumtot=0,
-            sumpos=0,
-            sumneg=0,
-            count=0;
+    int num, //Arbitrary number
+            sumtot=0, //Total sum of all values
+            sumpos=0, //Total sum of positive values only,
+            sumneg=0, //Total sum of negative values only
+            count=0;  //Controls number of times loop is repeated
+    //All values will not equal zero at end; just a control for now
    
         cout<<"Enter 10 arbitrary values, regardless of sign.\n";
         while (++count<=10)
     {
-            cin>>num;
+            cin>>num; 
         {
         if (num>0)
-             sumpos=sumpos+num;
+             sumpos=sumpos+num; //Total (+) sum
         else
-             sumneg=sumneg+num;
+             sumneg=sumneg+num; //Total (-) sum
         }
         
         sumtot=sumtot+num;
